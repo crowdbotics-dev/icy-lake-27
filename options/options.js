c4179c69faa1cfe8b01089acaa0e863da33722ca
Crowdbotics <engineering@crowdbotics.com>
2023-12-20 17:32:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-lake-27",
+  url: "https://icy-lake-27.botics.co",
+  api: "https://icy-lake-27.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
